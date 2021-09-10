1f75dd627317b5ec3c7312a381ffcda3b748e73c
Omar <omared250@gmail.com>
2021-09-11 00:11:28 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Logo/>
+    <BestPokemon/>
+  </div>
+)
+
+const Logo = () => (
+  <div>
+    <header>
+      <h1>Welcome to the Pokedex</h1>
+      <img src = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png' alt = '#'></img>
+    </header>
+  </div>
+)
+
+const BestPokemon = () => (
+  <p>My favorite pokemon is Charmander!</p>
+)
 
 export default App;
