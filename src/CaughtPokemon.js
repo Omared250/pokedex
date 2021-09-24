137b4f997a890efc3ffc5666ef0dd8664df2a7df
Omar <omared250@gmail.com>
2021-09-24 14:42:02 +0200

    exercise G inclass

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -3,23 +3,38 @@ import { useState } from "react";
 const CaughtPokemon = (props) => {
 
   const [caught, setCaught] = useState([]);
+  const [pokemonNameInput, setPokemonNameInput] = useState('');
 
   const catchPokemon = () => {
     // array of pokemon's name
-    const pokemons = ['Bulbasaur', 'Ivysaur', 'Charizard', 'Alakazam']
+    // const pokemons = ['Bulbasaur', 'Ivysaur', 'Charizard', 'Alakazam']
     // pick a random number of a pokemon name
-    const random = Math.floor(Math.random() * pokemons.length)
+    // const random = Math.floor(Math.random() * pokemons.length)
     // set the pokemon name
-    setCaught(caught.concat(pokemons[random]))
+    // if (pokemonNameInput === '') {
+    //   return window.alert('You need a pokemon name!')
+    // }
+    // else {
+    //   return setCaught(caught.concat(pokemonNameInput))
+    // }
+    setCaught(caught.concat(pokemonNameInput))
+    setPokemonNameInput('')
+  }
+
+  const handleInputChange = (event) => {
+    setPokemonNameInput(event.target.value)
   }
 
   return (<div>
     <p>Caught {caught.length} Pokémon on {props.date}</p>
     <ul>
       {caught.map((pokemon, index) => {
-        return <li key={index}>{pokemon}</li>
+        return <li key={index}>{pokemon === '' ? window.alert('You need a pokemon name!') : pokemon}</li>
       })}
     </ul>
+    <input type='text'
+      value={pokemonNameInput}
+      onChange={handleInputChange} />
     <button onClick={catchPokemon}>catch a Pokemon!</button>
   </div>
   );
